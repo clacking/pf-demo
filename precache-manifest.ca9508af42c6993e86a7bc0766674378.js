45aa55265ad267ff51491569d4f7325c138b0d29
clacking <click@clacking.net>
2019-03-12 18:40:34 +0900

    Updates

---
+++ precache-manifest.ca9508af42c6993e86a7bc0766674378.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5574de98dbee2198cc4",
+    "url": "/pf-demo/static/css/main.d1278171.chunk.css"
+  },
+  {
+    "revision": "f5574de98dbee2198cc4",
+    "url": "/pf-demo/static/js/main.7fa33cac.chunk.js"
+  },
+  {
+    "revision": "d93efcb05d3c209b4c7a",
+    "url": "/pf-demo/static/js/runtime~main.0c31d027.js"
+  },
+  {
+    "revision": "19c3641aa53680258556",
+    "url": "/pf-demo/static/js/2.06699244.chunk.js"
+  },
+  {
+    "revision": "3acb1181b9caf5e89b7f151e6e252478",
+    "url": "/pf-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cdd186e8a6f89de6605488c4933b09f1.js"
+  "/pf-demo/precache-manifest.ca9508af42c6993e86a7bc0766674378.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/pf-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
