983cbc92cb546b8a94d591280675fbd9b0bfca9c
clacking <click@clacking.net>
2019-03-12 18:36:22 +0900

    Updates

---
+++ precache-manifest.cdd186e8a6f89de6605488c4933b09f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34d1b68c351c4ea62d0e",
+    "url": "/static/css/main.d1278171.chunk.css"
+  },
+  {
+    "revision": "34d1b68c351c4ea62d0e",
+    "url": "/static/js/main.95afe223.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "19c3641aa53680258556",
+    "url": "/static/js/2.06699244.chunk.js"
+  },
+  {
+    "revision": "ecec242eeddb3d6860e93c0a37f8ea69",
+    "url": "/index.html"
+  }
+];
